86815245f865a32eeed18f272a784a75d6c6c9ce
Nick <pucillo.nick@gmail.com>
2018-04-23 22:33:42 -0400

    add edit functionality, refactor forms to partials, update articles controller functionality.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+# Add the Articles Helper class so that we can use 'article_params'
+  include ArticlesHelper
 
   def index
     @articles = Article.all
@@ -8,8 +10,36 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+# Creating Articles (new sends form data to create)
   def new
+    @article = Article.new
+  end
 
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to article_path(@article)
+  end
+
+# Updating Articles (edit sends form data to update)
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    redirect_to article_path(@article)
+  end
+
+# Deleting Articles
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,9 @@
 module ArticlesHelper
+# article_params gets used in our articles_controller, allowing us to pass the
+# full hash, but only permits the title and body params to be used, avoiding
+# error
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
 end
