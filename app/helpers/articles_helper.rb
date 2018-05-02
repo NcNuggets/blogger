02cd8bbfeec594b0edddcde89432db12aaf411dc
Nick <pucillo.nick@gmail.com>
2018-05-01 22:09:40 -0400

    Tagging feature complete

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -3,7 +3,7 @@ module ArticlesHelper
 # full hash, but only permits the title and body params to be used, avoiding
 # error
   def article_params
-    params.require(:article).permit(:title, :body)
+    params.require(:article).permit(:title, :body, :tag_list)
   end
 
 end
